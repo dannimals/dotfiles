33318c7ec0b9379ff4e02ff90878413c96d2fea5
Danning Ge <danning.ge@gmail.com>
2020-06-25 10:22:15 -0400

    Add aliases for fastlane

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,9 @@ alias dotfiles='cd ~/.dotfiles'
 
 # iOS
 alias blastDerivedData='rm -rf ~/Library/Developer/Xcode/DerivedData/*'
+
+alias be='bundle exec'
+
+alias bef='bundle exec fastlane'
+alias rgdev='bef register_devices_prompt'
+alias rgfile='bef register_devices_file'
