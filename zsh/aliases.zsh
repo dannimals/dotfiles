344cc164efac930c4bac513f9487ed5e080995fb
dannimals <danning.ge@gmail.com>
2026-04-27 15:42:14 -0400

    Fix zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,11 @@
+alias vs='open -a "/Applications/Visual Studio Code.app"'
 alias reload_me='. ~/.zshrc'
 alias zshrc='sbl ~/.zshrc'
 
 # iOS
 alias blastDerivedData='rm -rf ~/Library/Developer/Xcode/DerivedData/*'
 alias pi='pod install'
-alias sim ='xcrun simctl openurl booted'
+alias sim='xcrun simctl openurl booted'
 # VPNs
 alias my_ip="curl ifconfig.me"
 
